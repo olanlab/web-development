054af84b39db882bc973a4a0313262dacfbafb48
OLANLAB\notsk <olan@olanlab.com>
2018-01-04 13:01:29 +0700

    updated source code es5 es6

--- backend/javascript/es5_es6/modules/module.js
+++ backend/javascript/es5_es6/modules/module.js
@@ -1,10 +1,12 @@
 var math1 = require("./math.1.js");
 var math2 = require("./math.2.js");
 
+// solution 1 : math1
 console.log("1|", math1.add(1, 2));
 console.log("1|", math1.substract(1, 2));
 console.log("1|", math1.multiple(1, 2));
 
+// solution 2 : math2
 console.log("2|", math2.add(1, 2));
 console.log("2|", math2.substract(1, 2));
 console.log("2|", math2.multiple(1, 2));
