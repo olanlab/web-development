d9c5d600afea7bfe9a8df3230bc8275a1e3c61fe
Olan Samritjiarapon <olan@olanlab.com>
2018-01-05 14:10:27 +0700

    updated nodejs expressjs

--- backend/nodejs/express/server/routes/route.2.js
+++ backend/nodejs/express/server/routes/route.2.js
@@ -5,13 +5,10 @@ app.get("/example/a", (req, res) => {
 	res.send("Hello from A!");
 });
 // ----------------------------------------------------------------------------
-app.get(
-	"/example/b",
-	(req, res, next) => {
+app.get("/example/b", (req, res, next) => {
 		console.log("the response will be sent by the next function ...");
 		next();
-	},
-	(req, res) => {
+	}, (req, res) => {
 		res.send("Hello from B!");
 	}
 );
@@ -20,31 +17,24 @@ var cb0 = (req, res, next) => {
 	console.log("CB0");
 	next();
 };
-
 var cb1 = (req, res, next) => {
 	console.log("CB1");
 	next();
 };
-
 var cb2 = (req, res) => {
 	res.send("Hello from C!");
 };
 
 app.get("/example/c", [cb0, cb1, cb2]);
 // ----------------------------------------------------------------------------
-app.get(
-	"/example/d",
-	[cb0, cb1],
-	(req, res, next) => {
+app.get("/example/d", [cb0, cb1], (req, res, next) => {
 		console.log("the response will be sent by the next function ...");
 		next();
-	},
-	(req, res) => {
+	}, (req, res) => {
 		res.send("Hello from D!");
 	}
 );
 // ----------------------------------------------------------------------------
-
 app.listen(3000, function() {
 	console.log("Server listening on port 3000!");
 });
