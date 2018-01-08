e21658f8c1c0bb6ae272e0ef116cfcc69c35dde4
OLANLAB\notsk <olan@olanlab.com>
2018-01-08 18:55:58 +0700

    updated mongodb source code

--- backend/mongodb/mongoose_/find.4.js
+++ backend/mongodb/mongoose_/find.4.js
@@ -11,9 +11,17 @@ User.find().where('firstname').equals('Olan').exec((err, data) => {
     }
     console.log('User.find - firstname|', data);        // Array
 })
-User.find().where('age').gt(20).exec((err, data) => {
+// where, ne - not equal
+User.find().where('age').ne(33).exec((err, data) => {
     if(err) {
         console.log(err);
     }
-    console.log('User.find - age|', data);        // Array
+    console.log('User.find - age not equal |', data);        // Array
+})
+// where, in
+User.find().where('age').in([33]).exec((err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log('User.find - age in |', data);        // Array
 })
---
+++ backend/mongodb/mongoose_/find.5.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+mongoose.Promise = global.Promise;
+
+var User = require('./UserModel');
+// --------------------------------------------------------------------------------
+// where, gt - greater than
+User.find().where('age').gt(20).exec((err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log('User.find - age greater than |', data);        // Array
+})
+
+// where, lt - less than
+User.find().where('age').lt(20).exec((err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log('User.find - age less than |', data);        // Array
+})
