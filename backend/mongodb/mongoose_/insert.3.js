e83f207212e564416519113eabf4755cedc9a608
Olan Samritjiarapon <olan@olanlab.com>
2018-01-08 20:43:23 +0700

    updated mongoose source code

--- backend/mongodb/mongoose_/insert.3.js
+++ backend/mongodb/mongoose_/insert.3.js
@@ -1,5 +1,25 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
 mongoose.Promise = global.Promise;
 
-var User = require('./UserModel');
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+	firstname: { type: String, required: true },
+	lastname: String,
+	email: { type: String, required: true, trim: true, lowercase: true },
+    age: { type: Number, min: 1, max: 110,  default: 15},
+    created_date: {type: Date, default: Date.now}
+});
+
+var User = mongoose.model("User", userSchema);
+var person = new User({ firstname: "Seen", lastname: "Vechprasit", email: "zeza266@hotmail.com", age: 18});
+
+// --------------------------------------------------------------------------------
+person.save(function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("save successsfully.");
+	}
+});
+// --------------------------------------------------------------------------------
