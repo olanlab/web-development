51ab3d89bbf1b8e5e9b92fa5e6c4b901dbe7376b
OLANLAB\notsk <olan@olanlab.com>
2018-03-04 21:24:29 +0700

    updated source code

---
+++ frontend/javascript/fundamentals/string.1.js
@@ -0,0 +1,14 @@
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length;
+
+var str = "HELLO WORLD";
+var res = str.charAt(0);
+
+var str = "Please locate where 'locate' occurs!";
+var pos = str.indexOf("locate");
+
+var str = "Please locate where 'locate' occurs!";
+var pos = str.lastIndexOf("locate");
+
+var str = "Visit Microsoft!";
+var res = str.replace("Microsoft", "OlanLab");
